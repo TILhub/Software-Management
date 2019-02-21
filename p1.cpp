65b9dbbd89d982e24de3c62d7bed2ee5043ce56d
Sahil Malik <sahilmalikroorkee@gmail.com>
2019-02-21 15:59:03 +0530

    Add files via upload

--- p1.cpp
+++ p1.cpp
@@ -11,20 +11,23 @@ int countMatchInRegex(std::string s, std::regex re)
     return std::distance(words_begin, words_end);
 }
 int main(void){
+    system("cls");
     regex re_if("if(.*)");
     regex re_while("while(.*)");
     regex re_for("for(.*)");
-    fstream ifs("input.txt");
+    fstream ifs("input_p1.txt");
     string str;
     int c_if=0,c_while=0,c_for=0;
     while(ifs>>str){
-        cout<<str<<endl;
+        //cout<<str<<endl;
         c_if+=countMatchInRegex(str,re_if);
         c_while+=countMatchInRegex(str,re_while);
         c_for+=countMatchInRegex(str,re_for);
     }
-    cout<<"IF: "<<c_if<<endl;
-    cout<<"WHILE: "<<c_while<<endl;
-    cout<<"FOR: "<<c_for<<endl;
+    cout<<"\n\tif:\t"<<c_if<<endl;
+    cout<<"\n\twhile:\t"<<c_while<<endl;
+    cout<<"\n\tfor:\t"<<c_for<<endl;
+
+    cout<<"\n\tCyclocmetric Complexity: "<<c_if+c_while+c_for+1<<endl;
     return 0;
 }
